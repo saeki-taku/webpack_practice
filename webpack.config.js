84995c5552efebba9625307aeaa8b9519c33f07d
taku <63911366+saeki-taku@users.noreply.github.com>
2020-05-01 23:31:58 +0900

    ES6, React, Vue, etc...

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const path = require('path');
 const MiniCssExtarctPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/javascript/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -12,6 +15,30 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue/,
+        exclude: /node-modules/,
+        use: [
+          {
+            loader: 'vue-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { 'targets': '> 0.25%, not dead' }],
+                '@babel/preset-react',
+              ],
+            },
+          },
+        ],
+      },
+      {
         test: /\.(css|sass|scss)/,/**.css|sass|scssファイル名を検知するシステム */
         use: [
           {
@@ -19,6 +46,9 @@ module.exports = {
           },
           {
             loader: 'css-loader',
+            options: {
+              sourceMap: false,
+              }
           },
           {
             loader: 'sass-loader',
@@ -40,7 +70,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         use: [
           {
             loader: 'file-loader',
@@ -49,11 +79,21 @@ module.exports = {
               name: 'images/[name].[ext]',
             },
           },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
         ],
       },
     ],
   },
   plugins: [
+    new VueLoaderPlugin(),
     new MiniCssExtarctPlugin({
       filename: './stylesheets/main.css',
     }),
