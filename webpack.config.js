5d77f42da457afefbffee93ae541120c1bac7b88
taku <63911366+saeki-taku@users.noreply.github.com>
2020-05-02 18:07:36 +0900

    Add hash to filename

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/javascript/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'javascripts/main.js',
+    filename: 'javascripts/[name]-[hash].js',
   },
   module: {
     rules: [
@@ -75,7 +75,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name: 'images/[name].[ext]',
+              name: 'images/[name]-[hash].[ext]',
               publicPath: '/',
             },
           },
@@ -94,7 +94,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtarctPlugin({
-      filename: './stylesheets/main.css',
+      filename: './stylesheets/[name]-[hash].css',
     }),
     new HtmlWebpackPlugin({
       template: './src/template/index.pug',
